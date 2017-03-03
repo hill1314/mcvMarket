ee556f3a8eed7233abe086de5a336ebc8e4e245f
hill <467291935@qq.com>
2017-03-03 17:36:41 +0800

    update

---
+++ src/main/java/com/hull/interceptor/MyInterceptor.java
@@ -0,0 +1,54 @@
+package com.hull.interceptor;
+
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by hull on 2017/3/3.
+ */
+public class MyInterceptor implements HandlerInterceptor {
+
+    /**
+     * 在业务处理器处理请求之前被调用
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        return false;
+    }
+
+    /**
+     * 在业务处理器处理请求执行完成后,生成视图之前执行
+     * @param request
+     * @param response
+     * @param handler
+     * @param modelAndView
+     * @throws Exception
+     */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    /**
+     * 在DispatcherServlet完全处理完请求后被调用,可用于清理资源等
+     * @param request
+     * @param response
+     * @param handler
+     * @param ex
+     * @throws Exception
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
